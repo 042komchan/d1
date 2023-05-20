95104944c39fa42d0e44da4d0e413b63140119b6
042komchan <komchan.ying@mail.kmutt.ac.th>
2023-05-20 21:57:47 +0700

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int rows;
+
+        while (true)
+        {
+            Console.Write("Enter the number of rows (0 or greater): ");
+            if (int.TryParse(Console.ReadLine(), out rows))
+            {
+                if (rows >= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Pascal's triangle row number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Pascal's Triangle:");
+
+        for (int i = 0; i <= rows; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(CalculatePascalValue(i, j) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int CalculatePascalValue(int row, int col)
+    {
+        if (col == 0 || col == row)
+        {
+            return 1;
+        }
+        else
+        {
+            return CalculatePascalValue(row - 1, col - 1) + CalculatePascalValue(row - 1, col);
+        }
+    }
+}
+
